244693515bfd7664c6886de234aab91096321782
EggrollMama <annietran0213@gmail.com>
2023-03-28 01:27:05 -0500

    Fixed Ticket_Booth_Handler. Added a purchase date

--- Ticket_Booth_Handler.php
+++ Ticket_Booth_Handler.php
@@ -1,15 +1,17 @@
 <?php 
     include_once 'sqlConnect.php';
-
+    //takes input values from HTML identified by their names and puts them into global variable
+    $PurchaseDate = $_POST['date'];
     $QtyWheel = $_POST['QtyWheel']; 
     $QtySpeed = $_POST['QtySpeed']; 
     $QtyAqua = $_POST['QtyAqua']; 
     $QtyPutt = $_POST['QtyPutt']; 
 
-    $sql = "INSERT INTO ticket_booth(ticket_quantity) VALUES('$QtyWheel', '$QtySpeed', '$QtyAqua', '$QtyPutt');";
+    //call global variables to insert into table. User only inputs date, and the quantity of each ride they want to ride
+    $sql = "INSERT INTO ticket_booth(purchase_date, QtyWheel, QtySpeed, QtyAqua, QtyPutt) VALUES('$QtyWheel', '$QtySpeed', '$QtyAqua', '$QtyPutt');";
     mysqli_query($conn, $sql); 
 
-        
+    //search bar will show this message if the insert is successful 
     header("Location: ../Ticket_Booth.html?purchase=success");
 
 
