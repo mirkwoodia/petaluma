243a281ece11c48afefbadb77fb2d4bc0a53fe41
EggrollMama <annietran0213@gmail.com>
2023-03-27 22:23:31 -0500

    Connected Ticket_Booth to Database

---
+++ Ticket_Booth_Handler.php
@@ -0,0 +1,15 @@
+<?php 
+    include_once 'sqlConnect.php';
+
+    $QtyWheel = $_POST['QtyWheel']; 
+    $QtySpeed = $_POST['QtySpeed']; 
+    $QtyAqua = $_POST['QtyAqua']; 
+    $QtyPutt = $_POST['QtyPutt']; 
+
+    $sql = "INSERT INTO ticket_booth(ticket_quantity) VALUES('$QtyWheel', '$QtySpeed', '$QtyAqua', '$QtyPutt');";
+    mysqli_query($conn, $sql); 
+
+        
+    header("Location: ../Ticket_Booth.php?purchase=success");
+?>
+
---
+++ sqlConnect.php
@@ -0,0 +1,8 @@
+<?php 
+$dbServername ="mydb"; 
+$dbUsername = "root"; 
+$dbpassword = ""; 
+$dbname = "Themepark_Database"; 
+
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
