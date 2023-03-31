ceff438af1e0cfc46f6c35f4750f018df73cc0c6
mirkwoodia <smirkwoodia@gmail.com>
2023-03-31 01:10:22 +0000

    cloud re-infrastructuring

--- Ticket_Booth_Handler.php
+++ Ticket_Booth_Handler.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     include_once 'sqlConnect.php';
     //takes input values from HTML identified by their names and puts them into global variable
     $PurchaseDate = $_POST['date'];
@@ -8,10 +8,9 @@
     $QtyPutt = $_POST['QtyPutt']; 
 
     //call global variables to insert into table. User only inputs date, and the quantity of each ride they want to ride
-    $sql = "INSERT INTO ticket_booth(purchase_date, QtyWheel, QtySpeed, QtyAqua, QtyPutt) VALUES('$QtyWheel', '$QtySpeed', '$QtyAqua', '$QtyPutt');";
+    $sql = "INSERT INTO ticket_booth(purchase_date, QtyWheel, QtySpeed, QtyAqua, QtyPutt) VALUES('$PurchaseDate', '$QtyWheel', '$QtySpeed', '$QtyAqua', '$QtyPutt');";
     mysqli_query($conn, $sql); 
 
     //search bar will show this message if the insert is successful 
-    header("Location: Ticket_Booth.html?purchase=success");
-
-
+    header("Location:Ticket_Booth.html?purchase=success");
+?>
--- sqlConnect.php
+++ sqlConnect.php
@@ -1,7 +1,15 @@
-<?php 
-$dbServername ="localhost"; 
-$dbUsername = "root"; 
-$dbpassword = "Decon_0213"; 
-$dbname = "mydb"; 
+<?php
+    $dbServername ="localhost";
+    $dbUsername = "root";
+    $dbpassword = "Decon_0213";
+    $dbname = "mydb";
 
-$conn = mysqli_connect($dbServername, $dbUsername, $dbpassword, $dbname);
+    try {
+        $conn = new PDO("mysql:host=$dbServername;dbname=mydb", $dbUsername, $dbpassword);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
