9c4fc696e59002cb2a71dc5cb600371dcac8d16f
ArmandoCecilio <117837439+ArmandoCecilio@users.noreply.github.com>
2023-04-11 23:51:55 -0500

    Update Home_Page.php

--- Home_Page.php
+++ Home_Page.php
@@ -17,9 +17,9 @@ if (!isset($_SESSION['loggedin'])) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Curves, Waves, and Blobs</title>
+    <title>Petaluma</title>
 
-    <link rel="stylesheet" type="text/css" href="Home_Page.css">
+    <link rel="stylesheet" href="Home_Page.css">
 
 
   </head>
