fd41c0b7faf5764a755504c32ecb4cf8159ea5fa
ArmandoCecilio <117837439+ArmandoCecilio@users.noreply.github.com>
2023-04-13 00:19:50 -0500

    Update Parking_report.php

--- Parking_report.php
+++ Parking_report.php
@@ -12,7 +12,7 @@
 <title>Parking Table</title>
 </head>
 <body>
-<nav class="navtop">
+<nav class="navtop" style="margin-top:80px">
 			<div>
 				<h1>Petaluma Themepark Attractions</h1>
 				<a href="Admin_Portal.html"><i class="fas fa-user-circle"></i>Home</a>
@@ -20,47 +20,92 @@
 				<a href="Logout_Member.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
 		</nav>
-<table>
+<table style="margin-top:100px">
     <tr>
         <th>Parking lot name</th>
         <th>Total Parking Space</th>
         <th>Available Slots</th>
-        <th>Close Lot</th>
     </tr>
     <?php
     require_once('parking.php');
 
-    while($row = mysqli_fetch_assoc($r)) {
 
         $class = '';
-        if (isset($row['expiration_time'])) {
-            if (time() > $row['expiration_time']) {
-                $class = '';
-            } else {
-                $class = 'closed';
-            }
-        }
-        echo "<tr class='$class'>";
+
+
+        $STMT = "SELECT * FROM mydb.parking_slots;";
+        $r = mysqli_query($dbc, $STMT);
+        while($row = mysqli_fetch_assoc($r)) {
+        echo "<tr class='$class' style='background-color: " . ($class == "even" ? "#00ff84" : "#00ff84") . "; color: " . ($class == "even" ? "#333333" : "#000000") . ";'>";
         echo "<td>" . $row['lot_name'] . "</td>";
         echo "<td>" . $row['total_slots'] . "</td>";
         echo "<td>" . $row['available_slots'] . "</td>";
-        echo "<td>";
         echo "<form method='post'>";
         echo "<input type='hidden' name='lot_name' value='" . $row['lot_name'] . "'>";
-        echo "<input type='submit' name='close_parking' value='Close Parking'>";
         echo "</form>";
         echo "</td>";
         echo "</tr>";
 
-        $member_query = "SELECT m.*, gpp.parking_lot FROM get_parking_pass gpp 
+        
+    }
+
+    
+    ?>
+</table>
+
+
+
+
+
+</body>
+
+
+
+
+
+<br>
+
+
+<body>
+
+<table>
+    <tr>
+        <th>Parking lot name</th>
+        <th>Username</th>
+        <th>License Plate</th>
+    </tr>
+    <?php
+    require_once('parking.php');
+
+
+        $class = '';
+
+
+        $STMT = "SELECT * FROM mydb.parking_slots;";
+        $r = mysqli_query($dbc, $STMT);
+        while($row = mysqli_fetch_assoc($r)) {
+        echo "<tr class='$class' style='background-color: " . ($class == "even" ? "#00ff84" : "#00ff84") . "; color: " . ($class == "even" ? "#333333" : "#000000") . ";'>";
+        echo "<td>" . $row['lot_name'] . "</td>";
+        echo "<td></td>";
+
+        echo "<td></td>";
+        echo "<form method='post'>";
+        echo "<input type='hidden' name='lot_name' value='" . $row['lot_name'] . "'>";
+        echo "</form>";
+        echo "</td>";
+        echo "</tr>";
+
+        $member_query = "SELECT m.*, gpp.* FROM get_parking_pass gpp 
         JOIN member m ON gpp.member_ID = m.member_ID 
         WHERE gpp.parking_lot = '{$row['lot_name']}'";
         $member_result = mysqli_query($dbc, $member_query);
         if ($member_result) {
             while($member_row = mysqli_fetch_assoc($member_result)) {
                 echo "<tr>";
-                echo "<td></td><td></td><td></td>";
+                echo "<td>";
                 echo "<td>{$member_row['first_name']}</td>";
+                echo "<td>{$member_row['license_plate']}</td>";
+
                 echo "</tr>";
             }
         } else {
@@ -69,8 +114,16 @@
             echo "</tr>";
         }
     }
+
     mysqli_close($dbc);
+    
     ?>
 </table>
+
+
+
+
+
 </body>
+
 </html>
