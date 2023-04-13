f72c2bc98f261ef0fe3060d7a844f90f061d6695
ArmandoCecilio <117837439+ArmandoCecilio@users.noreply.github.com>
2023-04-13 00:21:20 -0500

    Update Home_Page.php

--- Home_Page.php
+++ Home_Page.php
@@ -29,6 +29,7 @@ if (!isset($_SESSION['loggedin'])) {
                 <?php if (isset($_SESSION['loggedin'])) { ?>      
                     <li><a href="attractions.php">Attractions</a></li>
                     <li><a href="Ticket_Booth.php">Ticket Booth</a></li>
+                    <li><a href="getParking.php">Get Parking</a></li>
                 <?php } ?>
                 <?php if (!isset($_SESSION['loggedin'])) { ?>
                     <li style="float:right"><a href="Login_Member.html">Member Login/Register</a></li>
