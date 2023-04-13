4898a6a64684a7751728cbe737d4c00765c24352
ArmandoCecilio <117837439+ArmandoCecilio@users.noreply.github.com>
2023-04-13 00:40:44 -0500

    Update Ticket_Booth.php

--- Ticket_Booth.php
+++ Ticket_Booth.php
@@ -17,7 +17,6 @@ if (!isset($_SESSION['loggedin'])) {
         <div>
             <h1>Petaluma Themepark</h1>
             <a href="Home_Page.php"><i class="fas fa-home"></i>Home</a>
-            <a href="Admin_Portal.html"><i class="fas fa-address-book"></i>More</a>
             <a href="Logout_Member.php"><i class="fas fa-address-book"></i>Logout</a>
         </div>
     </nav>
