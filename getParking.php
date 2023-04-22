41299f5ab978e78e0c288c68a1f1184fa1b34b3a
ArmandoCecilio <117837439+ArmandoCecilio@users.noreply.github.com>
2023-04-22 15:27:21 -0500

    Update getParking.php

--- getParking.php
+++ getParking.php
@@ -36,29 +36,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $parking_lot = $_POST['parking_lot'];
   $license_plate = $_POST['license_plate'];
 
-  $sql = "SELECT * FROM get_parking_pass WHERE member_ID = '$member_id'";
+  $sql = "SELECT * FROM get_parking_pass WHERE license_plate = '$license_plate' AND end_time > NOW()";
   $result = $conn->query($sql);
-
   if ($result->num_rows > 0) {
-    echo "Error: member with ID $member_id already has a parking pass";
+    $row = $result->fetch_assoc();
+    $end_time = strtotime($row['end_time']);
+    $time_left = $end_time - time();
+    $time_left_minutes = ceil($time_left / 60);
+    echo "Error: license plate $license_plate is already being used. Please try again in $time_left_minutes minute(s).";
     exit;
   }
 
   $start_time = date('Y-m-d H:i:s');
   $end_time = date('Y-m-d H:i:s', strtotime('+50 seconds', time()));
   $duration = strtotime($end_time) - strtotime($start_time);
-
+  $expiration_time = date('m/d/Y h:i:s A', strtotime($end_time . ' America/Chicago'));
+  
   $sql = "INSERT INTO get_parking_pass (member_ID, parking_lot, license_plate, start_time, end_time, duration, date_issued) VALUES ('$member_id', '$parking_lot', '$license_plate', NOW(), '$end_time', '$duration', NOW())";
-
+  
   if ($conn->query($sql) === TRUE) {
     echo "Parking pass assigned successfully!";
-    
-    echo "Your parking pass is valid for $duration seconds.";
-
+    echo "Your parking pass is valid until $expiration_time.";
+  
   } else {
     echo "Error assigning parking pass: " . $conn->error;
   }
-}
 ?>
 
 <style>
@@ -81,8 +83,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <option value="Lot D">Lot D</option>
   </select>
   <br><br>
-  <label for="license_plate">Enter your license plate:</label>
-  <input type="text" id="license_plate" name="license_plate" required>
+  <label for="license_plate">Enter your license plate (up to 7 characters):</label>
+  <input type="text" id="license_plate" name="license_plate" maxlength="7" pattern="[a-zA-Z0-9]{1,7}" required>
   <br><br>
   <input type="submit" value="Submit">
 </form>
